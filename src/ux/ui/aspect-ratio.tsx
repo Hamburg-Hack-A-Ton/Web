b1d2d72c0a80adeedea102f7a84c344e2976f393
DJL - Jack <71508487+JackatDJL@users.noreply.github.com>
2024-11-10 23:04:01 +0100

    rm ui

--- src/ux/ui/aspect-ratio.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
-// need to add smth because vercel git integration is not working
-
-const AspectRatio = AspectRatioPrimitive.Root;
-
-export { AspectRatio };
