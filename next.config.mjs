a49403d1e20fffd21f0fd62e572df4e78cc13340
DJL - Jack <71508487+JackatDJL@users.noreply.github.com>
2024-11-10 12:47:16 +0100

    try this

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,8 @@
 import {withSentryConfig} from "@sentry/nextjs";
 import { withVercelToolbar } from '@vercel/toolbar/plugins/next';
-import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        async_hooks: false,
-      };
-    }
-
-    config.module.rules.push({
-      test: /\.m?js/,
-      resolve: {
-        fullySpecified: false,
-      },
-    });
-
-    config.cache = false;
-
-    config.plugins.push(new NodePolyfillPlugin());
-
-    // Add a custom rule to handle node: scheme
-    config.module.rules.push({
-      test: /\.js$/,
-      loader: 'string-replace-loader',
-      options: {
-        search: 'node:',
-        replace: '',
-      },
-    });
-
-    return config;
-  }
 }
 
 
